71024a9ad5fb0fb490e8e39841b16fa63d195222
Malcolm Bacon <mbacon23@uwo.ca>
2020-11-01 23:53:42 -0500

    Added some basic property drawers

---
+++ Unity3DToolsTutorial/Assets/__Scripts/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("Character Stats")]
+    [SerializeField]
+    private int maxHealth = 100;
+
+    [ContextMenuItem("Choose Random DPS", "RandomizeDPS")]
+    [SerializeField]
+    [Range(1, 10)]
+    private int damagePerSecond = 3;
+
+    [Header("Character Description")]
+    [SerializeField]
+    private string characterName;
+
+    [SerializeField]
+    [TextArea]
+    //Could also do [Multiline]
+    private string characterDescription;
+    private int currentHealth;
+    [ContextMenu("TakeDamage")]
+    public void TakeDamage()
+    {
+        currentHealth--;
+    }
+    private void RandomizeDPS()
+    {
+        damagePerSecond = Random.Range(1, 10);
+    }
+}
